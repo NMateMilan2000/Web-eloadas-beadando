ec538040be8d555aff53eaa4c3b81f2b65312dd7
NMateMilan2000 <n.mate.milan@gmail.com>
2025-04-17 17:35:11 +0200

    szöveg javítás

--- 7feladat/src/home.jsx
+++ 7feladat/src/home.jsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div>
       <h1>Üdvözöljük az oldalon!</h1>
-      <p>Ez egy egyszerű Blackjack játék, ahol játszhatsz a gép ellen.</p>
+      <p>Ez egy egyszerű ahol  Blackjack játékot és Kő-Papír-Olló játékot lehet játszani.</p>
       <p>Kattints a "Blackjack" linkre a játék indításához!</p>
+      <p>Kattints a "RPS" linkre a Kő-Papír-Olló játék indításához!</p>
+      <p>Kattints a "Vissza" linkre a főoldalra való visszatéréshez!</p>
     </div>
   );
 }
